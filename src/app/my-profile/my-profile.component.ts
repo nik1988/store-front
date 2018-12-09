214908683a15f0a51092ece3026e94da5bd858c0
nik1988 <kamaninikhil71@gmail.com>
2018-12-09 23:30:43 +0530

    Mistakes and Learnings folder added my profile folder

---
+++ src/app/my-profile/my-profile.component.ts
@@ -0,0 +1,79 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "../model/user";
+import {UserService} from "../services/user.service";
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.css']
+})
+export class MyProfileComponent implements OnInit {
+
+  datafetched: boolean;
+
+  selectProfileTab: number;
+  userNameExists: boolean;
+  currentPassword: string;
+  emailExists: boolean;
+  txtNewPassword: string;
+  txtConfirmPassword: string;
+  anonymousUser: boolean = false;
+
+  errorMessage: string = "";
+  updateError: boolean;
+
+  user: User = new User();
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+
+    this.userService.getCurrentUser().subscribe(
+      (user: User) => {
+        this.user = user
+        console.log(user);
+        if (user == null) {
+          this.anonymousUser = true;
+          console.log("anonymous user");
+        }
+      })
+
+  }
+
+  onUpdateUserInfo() {
+
+
+    this.datafetched = false;
+    this.updateError = false;
+    this.userService.updateUser(this.user, this.txtNewPassword).subscribe(
+      (response) => {
+
+        this.datafetched = true;
+      },
+      (error) => {
+
+        console.log("error while updating user data");
+
+        let errormessage = error.error
+        this.updateError = true;
+
+        if (errormessage === 'incorrectPassword') {
+          this.errorMessage = "Incorrect Password !! Please enter correct password to update user details.";
+        }
+
+        if (errormessage === 'emailNotFound') {
+          this.errorMessage = "Incorrect Email address !! Please enter correct email to update user details.";
+        }
+
+        if (errormessage === 'userNameNotFound') {
+          this.errorMessage = "Incorrect UserName!! Please enter correct username";
+        }
+
+      }
+    )
+
+
+  }
+
+}
